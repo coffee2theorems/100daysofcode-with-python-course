dfae4353d2cd8ed0586b662122eb2a40affdd788
Ryan Monaghan <coffee2theorems314@gmail.com>
2018-11-24 18:01:35 -0600

    Notes for Day1

---
+++ my_stuff/1-3/datetimenotes.py
@@ -0,0 +1,67 @@
+"""
+Author: Ryan Monaghan
+Date: 11/24/2018
+Notes: Day 1 of 100 days of Code
+"""
+
+from datetime import datetime
+from datetime import date
+
+print(datetime.today()) #Year Month Day HR, MIN, SEC, MILLI
+
+today = datetime.today()
+print(type(today)) # <class 'datetiem.date'>
+
+todaydate = date.today()
+print(type(todaydate)) #<class 'datetime.date'>
+print(todaydate) #YEAR Month Day
+
+print(todaydate.month)
+print(todaydate.day)
+print(todaydate.year)
+
+christmas = date(2018, 12, 25)
+
+print(christmas - todaydate) #todaydate.timedelta(31)
+
+(christmas -todaydate).days #31
+
+if christmas is not todaydate:
+    print("Sorry there are still", (christmas-todaydate).days, "days until christmas")
+else:
+    print("Yay it's christmas")
+
+
+
+###Time Delta Notes - A gap in time###
+
+from datetime import timedelta
+
+t = timedelta(days = 4, hours = 10)
+print(type(t))
+
+print(t.days)
+print(t.seconds) # goes up to 1 day, gives us the seconds in our hours
+
+#t.hours doesn't work...
+
+print(t.seconds / 60 / 60) #seconds to hours
+
+
+eta = timedelta(hours = 6)
+today = datetime.today()
+
+print(today)
+print(eta)
+
+print(today + eta) # can add timedeltas to datetime
+
+
+### Play Time ###
+
+rightnow = datetime.today()
+future = timedelta(hours = 6)
+
+print("6 hours from now is", rightnow + future)
+print("6 hours in the past is", rightnow - future)
+
